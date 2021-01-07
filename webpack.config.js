4c5402192730cea38ee95199b855c7fdcb0e14d6
varun-maraka <varun.maraka@gmail.com>
2021-01-06 21:36:52 -0600

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    mode:'development',
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3001
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './dev/js/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    output: {
+        path: 'src',
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
